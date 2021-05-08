8f97d8a27274b1f575a789eb7cd9cd44e3ff51c0
sk2111 <sathisht1996@gmail.com>
2021-05-08 19:43:18 +0530

    Add: Equation and result added
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const calcButtons = [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 'c', 0, '=', '/'];
-
+let currentEquation = '';
 //Helper to create DOM node
 const createDOMNode = (tagName, content = '', attribute = []) => {
     const elem = document.createElement(tagName);
@@ -13,9 +13,11 @@ const createDOMNode = (tagName, content = '', attribute = []) => {
     return elem;
 };
 
-
+//creating DOM nodes for calculator
 const calcContainerNode = createDOMNode('div', '', [{ name: 'class', value: 'calc-container' }]);
 const userDisplayNode = createDOMNode('div', '', [{ name: 'class', value: 'user-display' }]);
+const resultDisplayNode = createDOMNode('div', '', [{ name: 'class', value: 'result-display ' }]);
+const gridContainer = createDOMNode('div', '', [{ name: 'class', value: 'grid-display ' }]);
 
 const buttonNodes = calcButtons.map((buttonText) => {
     const buttonClass = typeof (buttonText) === 'string' ? 'operator' : 'number';
@@ -26,6 +28,28 @@ const buttonNodes = calcButtons.map((buttonText) => {
     ]);
 });
 
-calcContainerNode.append(userDisplayNode, ...buttonNodes);
-
+// Add contents to DOM
+gridContainer.append(...buttonNodes);
+calcContainerNode.append(userDisplayNode, resultDisplayNode, gridContainer);
 document.body.append(calcContainerNode);
+
+//handlers for button click
+
+const handleButtonClick = (event, selectedButton) => {
+    const value = selectedButton ? selectedButton : event.getAttribute('data-value');
+    if (value === 'c') {
+        currentEquation = '';
+        userDisplayNode.innerText = '';
+        resultDisplayNode.innerText = '';
+
+    }
+    else if (value === '=') {
+        const output = eval(currentEquation);
+        resultDisplayNode.innerText = output;
+    }
+    else if (isFinite(value) || isFinite(currentEquation[currentEquation.length - 1])) {
+        currentEquation += value;
+        userDisplayNode.innerText = currentEquation;
+        resultDisplayNode.innerText = '';
+    }
+};
